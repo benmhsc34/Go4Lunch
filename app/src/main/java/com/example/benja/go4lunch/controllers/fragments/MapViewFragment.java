e1134ca2adb7428bc4f5e9e2fb26c69c0ffd99ba
Benjamin Corben <corbenbenjamin@gmail.com>
2019-01-26 11:23:50 +0100

    Markers changing color but not displaying the markers after

--- app/src/main/java/com/example/benja/go4lunch/controllers/fragments/MapViewFragment.java
+++ app/src/main/java/com/example/benja/go4lunch/controllers/fragments/MapViewFragment.java
@@ -50,7 +50,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  *  IN = Last Know Location : Location
  *
  **************************************************************************************************/
-public class MapViewFragment extends BaseFragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+public class MapViewFragment extends BaseFragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMarkerClickListener {
 
     // For debug
     private static final String TAG = MapViewFragment.class.getSimpleName();
@@ -84,9 +84,16 @@ public class MapViewFragment extends BaseFragment implements OnMapReadyCallback,
     String imageReferences;
     private CollectionReference notebookRef = FirebaseFirestore.getInstance().collection("utilisateurs");
     String restaurantNameFirebaseString;
+    int finalI;
 
 
     Marker marker;
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d("logloglog", marker.getTag().toString());
+        return true;
+    }
 
     // ==> CallBack
     // Interface for ShowSnakeBar
@@ -179,6 +186,10 @@ public class MapViewFragment extends BaseFragment implements OnMapReadyCallback,
         mMap.setOnInfoWindowClickListener(this);
 
 
+        //
+        mMap.setOnMarkerClickListener(this);
+
+
         Retrofit retrofit = new Retrofit.Builder().baseUrl(Api.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
 
         Api api = retrofit.create(Api.class);
@@ -200,29 +211,41 @@ public class MapViewFragment extends BaseFragment implements OnMapReadyCallback,
                     } else {
                         photoReferences = "CmRaAAAA0-j6NJjMJf_0-AXUEIl2CFiU1djE4V5inVAiHFXafJILjxZiLLisEdQDx_m9133Pbe2TWPJ_KVhyTQSHW_4J_LmkGKmgwoTphY9Ul1vO8dbd4oFXbzb8zEz7eK751glhEhBLRvmxTtdf6gOhkX2Y9_4IGhSbVmaZ8vWI3o3oVrzjGehz6Ck1zA";
                     }
+                }
 
+                notebookRef.addSnapshotListener((queryDocumentSnapshots, e) -> {
+                    for (QueryDocumentSnapshot userSnapshot : queryDocumentSnapshots) {
+                        for (PlaceNearBySearchResult result : theListOfResults) {
 
-                    int finalI = i;
-                    notebookRef.addSnapshotListener((queryDocumentSnapshots, e) -> {
-                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                            restaurantNameFirebaseString = documentSnapshot.getString("restaurantName");
+                            marker = mMap.addMarker(new MarkerOptions().position(new LatLng(result.getGeometry().getLocation().getLat(), result.getGeometry().getLocation().getLng()))
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_restaurant_marker_orange)));
+                            marker.setTag(result.getName());
+
+                            restaurantNameFirebaseString = userSnapshot.getString("restaurantName");
                             if (restaurantNameFirebaseString != null) {
-                                if (restaurantNameFirebaseString.equals(theListOfResults.get(finalI).getName())) {
+                                Log.d("restaurantNameFirebase", restaurantNameFirebaseString);
+                                Log.d("restaurantName", result.getName());
+                                if (restaurantNameFirebaseString.equals(result.getName())) {
 
-                                    marker = mMap.addMarker(new MarkerOptions().position(new LatLng(theListOfResults.get(finalI).getGeometry().getLocation().getLat(), theListOfResults.get(finalI).getGeometry().getLocation().getLng()))
+                                    marker = mMap.addMarker(new MarkerOptions().position(new LatLng(result.getGeometry().getLocation().getLat(), result.getGeometry().getLocation().getLng()))
                                             .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_restaurant_marker_green)));
-                                } else {
-                                    marker = mMap.addMarker(new MarkerOptions().position(new LatLng(theListOfResults.get(finalI).getGeometry().getLocation().getLat(), theListOfResults.get(finalI).getGeometry().getLocation().getLng()))
-                                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_restaurant_marker_orange)));
+                                    marker.setTag(result.getName());
+
+                                    break;
                                 }
                             } else {
-                                marker = mMap.addMarker(new MarkerOptions().position(new LatLng(theListOfResults.get(finalI).getGeometry().getLocation().getLat(), theListOfResults.get(finalI).getGeometry().getLocation().getLng()))
+                                marker = mMap.addMarker(new MarkerOptions().position(new LatLng(result.getGeometry().getLocation().getLat(), result.getGeometry().getLocation().getLng()))
                                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_restaurant_marker_orange)));
+                                marker.setTag(result.getName());
+
+
                             }
                         }
-                    });
-                }
+                    }
+
+                });
             }
+
 
             @Override
             public void onFailure(Call<PlaceNearBySearch> call, Throwable t) {
